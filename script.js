8d1f0d5d8979920291f63f9bee3dd186cb25737d
YingWaCollege-Student <s20220025@yingwa.edu.hk>
2024-04-20 20:04:09 +0800

    Modified style and script.

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-var num = Math.floor(Math.random() * 100);
-var min_num = 0;
-var max_num = 100;
-var range = document.getElementById("range");
-var guess_times = 0;
-var guessed_times = document.getElementById("gs");
+var num = Math.floor(Math.random() * 100); // Initialise the number to be guessed
+var min_num = 0; // Initialise the minimum number
+var max_num = 100; // Initialise the maximum number
+var range = document.getElementById("range"); // Get text element showing the number range
+var guess_times = 0; // Initialise guess times
+var guessed_times = document.getElementById("gs"); // Get text element showing guessed times
 
-function guess() {
-    let input = parseInt(document.getElementById("num_input").value);
-    if (input == num) {
+function guess() { // Function to process user input, triggered after button pressed or enter key pressed
+    let input = parseInt(document.getElementById("num_input").value); // Get input value
+    if (input == num) { // Winning case
         var confirmed = confirm(`Congratulations! You have won the game! Play again? 
 Total guessed times: ${guess_times}`);
         if (confirmed == true) {
-            window.location.reload();
+            window.location.reload(); // Restart game
         }
-    } else if (input < num && input < 100 && input < max_num && input > min_num) {
+    } else if (input < num && input < 100 && input < max_num && input > min_num) { // Smaller than number to be guessed case
         min_num = input;
         range.textContent = `${min_num} to ${max_num}! Guess again!`;
         guess_times ++;
         guessed_times.textContent = `Guessed times: ${guess_times}`;
-    } else if (input > num && input < 100 && input < max_num && input > min_num) {
+        num_input.value = "";
+    } else if (input > num && input < 100 && input < max_num && input > min_num) { // Larger than number to be guessed case
         max_num = input;
         range.textContent = `${min_num} to ${max_num}! Guess again!`;
         guess_times ++;
         guessed_times.textContent = `Guessed times: ${guess_times}`;
-    } else {
+        num_input.value = "";
+    } else { // Invalid case
         range.textContent = `${min_num} to ${max_num}! Invalid number!`;
+        num_input.value = "";
     }
 }
+
+let num_input = document.getElementById("num_input");
+
+num_input.addEventListener("keypress", function(event) { // Enter key event listener
+        if (event.key === "Enter") {
+          guess();
+        }
+      });
